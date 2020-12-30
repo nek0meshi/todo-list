1dd5a3be0e769054a4e31edab3801a5d37aec8a2
nek0meshi <nekmeshi@gmail.com>
2020-12-30 12:07:06 +0900

    - 編集モーダルの追加

---
+++ frontend/src/components/EditModal.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="edit-modal modal" :class="{ 'is-active': show }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">タスクを編集</p>
+        <button
+          class="delete"
+          aria-label="close"
+          @click="close"
+        ></button>
+      </header>
+      <section class="modal-card-body">
+        <input
+          type="text"
+          class="input"
+          v-model="innerText"
+        >
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button is-success" @click="save">保存</button>
+        <button class="button" @click="close">キャンセル</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+    text: {
+      type: String,
+      default: '',
+    }
+  },
+
+  data() {
+    return {
+      innerText: '',
+    }
+  },
+
+  watch: {
+    show(val) {
+      if (val) {
+        this.innerText = this.text
+      }
+    }
+  },
+
+  methods: {
+    save() {
+      this.$emit('save', {
+        text: this.innerText
+      })
+    },
+    close() {
+      this.$emit('close')
+    },
+  }
+}
+</script>
---
+++ frontend/src/components/Page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from './TodoList.vue'
+
+export default {
+  components: {
+    TodoList,
+  },
+}
+</script>
