e0908d0f95c40e863b67b5406641a814cf9f0188
nek0meshi <nekmeshi@gmail.com>
2022-08-15 12:09:54 +0900

    add .stylelintrc.js

---
+++ frontend/.stylelintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss',
+  ],
+  overrides: [
+    {
+      files: [
+        '*.vue',
+        '**/*.vue',
+      ],
+      extends: [
+        'stylelint-config-recommended-vue',
+      ],
+    },
+  ],
+}
